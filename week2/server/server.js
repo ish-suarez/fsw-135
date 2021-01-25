6a6913054be84f0b46237d80265c207237b2462b
Ismael Suarez <spawn@Ismaels-MacBook-Pro.local>
2021-01-24 22:44:39 -0600

    week2 server

---
+++ week2/server/server.js
@@ -0,0 +1,26 @@
+// Express Server
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+// Server Middleware
+app.use(express.json());
+app.use(morgan('dev'));
+
+// Mongoose Middleware
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/inventorydb',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    },
+    () => console.log("Connected to the DB")
+);
+
+// Inventory Route
+app.use('/inventory', require('./routes/inventoryRouter'));
+
+// App Set To Listen
+app.listen(9000, () => console.log('Server Is Listening On Port 9000'));
